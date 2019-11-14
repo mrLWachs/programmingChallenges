ee08aa5dff0ab825dd41cc6e924b466682ef9613
Mr. Wachs <mrLWachs@gmail.com>
2019-11-13 21:58:03 -0600

    The in-class examples

--- src/data/week2/Week2Level4Run.java
+++ src/data/week2/Week2Level4Run.java
@@ -1,13 +1,31 @@
 
 package data.week2;
 
+import io.System;
 import mainpackage.Problem;
 
 public class Week2Level4Run  extends Problem
 {
 
-   public void start() {
-       
+   public void start() {      
+        System.out.header("Week 2, Level 4: starting");
+        String[] data = {
+            "2",
+            "10",
+            "3 2 2 1 4 7 1 2 0 1",
+            "5",
+            "1 1 1 1 1"
+        };
+        System.out.println("Inputs:\n");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
+        System.out.println("\nOutputs:\n");
+        
+        
+        
+        System.out.header("Week 2, Level 4: complete");
+        System.flush();
    }
 
 }
