8432e6b73b480754344f18e163daae77b23fd406
Mr. Wachs <mrLWachs@gmail.com>
2019-12-03 00:45:57 -0600

    The in-class examples

--- src/challenge/week3/Week3Level4.java
+++ src/challenge/week3/Week3Level4.java
@@ -16,11 +16,62 @@ public class Week3Level4 extends Problem
         Scanner input = new Scanner(System.in); 
         String line = input.nextLine();
         while (!line.equals("0")) {
-            int dimension = Integer.parseInt(line);
-            
-            
+            int n = Integer.parseInt(line);
+            int size = 2*n+1;
+            char[][] grid = new char[size][size];
+            for (int i = 0; i < size; i++) {
+                line = input.nextLine();
+                char[] symbols = line.toCharArray();
+                grid[i] = symbols;
+            }       
+            String text = "";
+            for (int row = 0; row < grid.length; row++) {
+                for (int column = 0; column < grid[row].length; column++) {
+                    char symbol = grid[row][column]; 
+                    if (row    % 2 != 0 && 
+                        column % 2 != 0 &&
+                        symbol == ' ') {
+                        text += countEdges(row,column,grid);
+                    }
+                    else {
+                        text += symbol;
+                    }
+                }
+                text += "\n";
+            }
+            System.out.println(text);
             line = input.nextLine();
         }        
     }
+    
+    private String countEdges(int r, int c,char[][] grid) {
+        int count = 0;
+        if (inBounds(r-1, c  ,grid)) {
+            if (grid[r-1][c  ] == '_') count++;
+            if (grid[r-1][c  ] == '|') count++;            
+        }
+        if (inBounds(r  , c-1,grid)) {
+            if (grid[r  ][c-1] == '_') count++;
+            if (grid[r  ][c-1] == '|') count++;            
+        }
+        if (inBounds(r  , c+1,grid)) {
+            if (grid[r  ][c+1] == '_') count++;
+            if (grid[r  ][c+1] == '|') count++;            
+        }
+        if (inBounds(r+1, c  ,grid)) {
+            if (grid[r+1][c  ] == '_') count++;
+            if (grid[r+1][c  ] == '|') count++;
+        }
+        return "" + count;
+    }
 
+    private boolean inBounds(int r, int c, char[][] grid) {
+        int max = grid.length-1;
+        if (r < 0)   return false;
+        if (c < 0)   return false;
+        if (r > max) return false;
+        if (c > max) return false;
+        return true;
+    }
+    
 }
--- src/data/week3/Week3Level4Run.java
+++ src/data/week3/Week3Level4Run.java
@@ -11,7 +11,31 @@ public class Week3Level4Run  extends Problem
     public void start() {   
         System.out.header("Week 3, Level 4: starting");          
         String[] lines = { 
-           "under development",         
+           "3",
+           "._._._.",
+           "|     |",
+           ". ._. .",
+           "| | | |",
+           "._. ._.",
+           "       ",
+           "       ",
+           "7",
+           "._._.   ._.    ",
+           "|   |   | |    ",
+           "._. .   . ._.  ",
+           "  | |   |   |  ",
+           "._. ._._.   ._.",
+           "|             |",
+           "._. ._._.   ._.",
+           "  | |   |   |  ",
+           "._. .   .   .  ",
+           "|   |   |   |  ",
+           ".   .   .   ._.",
+           "|   |   |     |",
+           ".   .   ._._._.",
+           "|   |          ",
+           "._._.          ",
+           "0"           
         };
         System.out.println("Inputs:\n");
         for (int i = 0; i < lines.length; i++) {
@@ -20,10 +44,38 @@ public class Week3Level4Run  extends Problem
         System.out.println("\nOutputs:\n"); 
         ArrayList<String> outputs = new ArrayList<>();        
         for (int testCase = 0; testCase < lines.length; testCase++) {      
-            String text = lines[testCase];       
+            String line = lines[testCase];       
             
+            while (!line.equals("0")) {
+                int n = Integer.parseInt(line);
+                int size = 2*n+1;
+                char[][] grid = new char[size][size];
+                for (int i = 0; i < size; i++) {
+                    testCase++;
+                    line = lines[testCase];
+                    char[] symbols = line.toCharArray();
+                    grid[i] = symbols;
+                }       
+                String text = "";
+                for (int row = 0; row < grid.length; row++) {
+                    for (int column = 0; column < grid[row].length; column++) {
+                        char symbol = grid[row][column]; 
+                        if (row    % 2 != 0 && 
+                            column % 2 != 0 &&
+                            symbol == ' ') {
+                            text += countEdges(row,column,grid);
+                        }
+                        else {
+                            text += symbol;
+                        }
+                    }
+                    text += "\n";
+                }
+                outputs.add(text);
+                testCase++;
+                line = lines[testCase];
+            }      
             
-            outputs.add(text);
         }        
         for (int i = 0; i < outputs.size(); i++) {
             System.out.println(outputs.get(i));
@@ -32,4 +84,34 @@ public class Week3Level4Run  extends Problem
         System.flush();
     }
 
+    private String countEdges(int r, int c,char[][] grid) {
+        int count = 0;
+        if (inBounds(r-1, c  ,grid)) {
+            if (grid[r-1][c  ] == '_') count++;
+            if (grid[r-1][c  ] == '|') count++;            
+        }
+        if (inBounds(r  , c-1,grid)) {
+            if (grid[r  ][c-1] == '_') count++;
+            if (grid[r  ][c-1] == '|') count++;            
+        }
+        if (inBounds(r  , c+1,grid)) {
+            if (grid[r  ][c+1] == '_') count++;
+            if (grid[r  ][c+1] == '|') count++;            
+        }
+        if (inBounds(r+1, c  ,grid)) {
+            if (grid[r+1][c  ] == '_') count++;
+            if (grid[r+1][c  ] == '|') count++;
+        }
+        return "" + count;
+    }
+
+    private boolean inBounds(int r, int c, char[][] grid) {
+        int max = grid.length-1;
+        if (r < 0)   return false;
+        if (c < 0)   return false;
+        if (r > max) return false;
+        if (c > max) return false;
+        return true;
+    }
+    
 }
