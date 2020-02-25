d7a583437004a8521a1eab2de79fab451cad6606
Mr. Wachs <mrLWachs@gmail.com>
2020-02-25 15:28:31 -0600

    Newest one

--- src/data/week10/Week10Level3Run.java
+++ src/data/week10/Week10Level3Run.java
@@ -40,63 +40,74 @@ public class Week10Level3Run  extends AutoRun
     }
     
     private int eliminatedTeams;
-    private int rounds;
     
     
-    private void process(int numberOfTeams) {
-        rounds = 0;
-        ArrayList<Team> teams = new ArrayList<>();
-        for (int i = 0; i < numberOfTeams; i++) {
-            teams.add(new Team());
-        }
-        while (eliminatedTeams < numberOfTeams) {
-            playRound(teams);
+    private void process(int number) {
+        int round = 0;
+        ArrayList<Team> teams = createTeams(number);
+        while (eliminatedTeams < number) {
+            playRound(teams,round);
+            round++;
         }        
+        System.out.println("There are " + round + " rounds.");
     }
     
-    private void playRound(ArrayList<Team> teams) {        
-        int undefeatedTeams = 0;
-        int oneLossTeams    = 0;
-        for (int i = 0; i < teams.size(); i++) {
-            Team team = teams.get(i);
-            if (team.undefeated)  undefeatedTeams++;
-            if (team.losses == 1) oneLossTeams++;
+    private ArrayList<Team> createTeams(int number) {
+        ArrayList<Team> teams = new ArrayList<>();
+        for (int i = 0; i < number; i++) {
+            teams.add(new Team());
         }
-        System.out.println("Round " + rounds + ": " + 
-                undefeatedTeams + " undefeated, " + 
-                oneLossTeams    + " one-loss, " + 
-                eliminatedTeams + " eliminated");
+        return teams;
+    }
+    
+    private void playRound(ArrayList<Team> teams, int round) {        
+        showRound(teams,round);
+
         ArrayList<Team> activeTeams = new ArrayList<>();
         for (int i = 0; i < teams.size(); i++) {
             Team team = teams.get(i);
-            if (!team.eliminated) {
+            if (!team.isEliminated()) {
+                team.dontChoose();
                 activeTeams.add(team);
             }
-        }
-        for (int i = 0; i < activeTeams.size(); i++) {
-            activeTeams.get(i).chosen = false;
+            
+//            show(teams, "Added active team");
+            
         }
         boolean lastRound = createPairings(activeTeams);
         if (lastRound) {
             for (int i = 0; i < activeTeams.size(); i++) {
                 Team team = activeTeams.get(i);
-                if (!team.eliminated) {
-                    team.eliminated = true;
+                if (!team.isEliminated()) {
+                    team.eliminate();
                     eliminatedTeams++;
                 }
             }
         }
-        rounds++;
     }
 
+    private void showRound(ArrayList<Team> teams, int round) {
+        int undefeatedTeams = 0;
+        int oneLossTeams    = 0;
+        for (int i = 0; i < teams.size(); i++) {
+            Team team = teams.get(i);
+            if (team.isUndefeated())   undefeatedTeams++;
+            if (team.getLosses() == 1) oneLossTeams++;
+        }        
+        System.out.println("Round " + round + ": " + 
+                undefeatedTeams + " undefeated, " + 
+                oneLossTeams    + " one-loss, " + 
+                eliminatedTeams + " eliminated");
+    }
+    
     private boolean createPairings(ArrayList<Team> teams) {
         for (int i = 0; i < teams.size(); i++) {
             Team team = teams.get(i);
-            if (!team.chosen) {
-                team.chosen = true;
+            if (!team.isChosen()) {
+                team.choose();
                 Team opponent = findOpponent(teams,team);
                 if (opponent == null) return true;
-                opponent.chosen = true;
+                opponent.choose();
                 opponent.lose();
                 return false;
             }
@@ -113,40 +124,108 @@ public class Week10Level3Run  extends AutoRun
     }
 
     private boolean canPlay(Team team, Team opponent) {
-        if (opponent.chosen)                             return false;
-        if (team.undefeated     && opponent.losses == 1) return false;
-        if (opponent.undefeated && team.losses     == 1) return false;
+        if (opponent.isChosen())                                  return false;
+        if (team.isUndefeated()     && opponent.getLosses() == 1) return false;
+        if (opponent.isUndefeated() && team.getLosses()     == 1) return false;
         return true;
     }
 
+    private void show(ArrayList<Team> teams, String text) {
+        System.out.println(text + "...................................");
+        for (int i = 0; i < teams.size(); i++) {
+            Team team = teams.get(i);
+            System.out.println(team.toString());
+        }
+        System.out.println(".........................................\n");
+    }
+    
+    
+    
+    
+    
+    private static int teams = 0;
+
+    
+
+    
     
     private class Team 
     {
         
-        public int     losses;
-        public boolean eliminated;
-        public boolean undefeated;        
-        public boolean chosen;
+        private int     number;
+        private int     losses;
+        private boolean eliminated;
+        private boolean undefeated;        
+        private boolean chosen;
         
         
         public Team() {
             losses     = 0;
             eliminated = false;
             undefeated = true;
-            chosen     = false;            
+            chosen     = false; 
+            teams++;
+            number = teams;
+            
+            show("created!");
         }
         
-        public void lose() {
-            if (!eliminated) {
-                System.out.println("Loss!");
-                losses++;
-                undefeated = false;
-                if (losses == 2) {
-                    System.out.println("Eliminated!");
-                    eliminated = true;
-                    eliminatedTeams++;
-                }
-            }
+        public int getLosses() {
+            return losses;
+        }
+        
+        public boolean isEliminated() {
+            return eliminated;
+        }
+        
+        public void eliminate() {
+            eliminated = true;
+            eliminatedTeams++;
+            
+            show("eliminate");
+        }
+        
+        public boolean isUndefeated() {
+            return undefeated;
+        }
+        
+        public boolean isChosen() {
+            return chosen;
+        }
+        
+        public void choose() {
+            chosen = true;
+            
+            
+            show("choosing!");
+        }
+        
+        public void dontChoose() {
+            chosen = false;
+            
+            show("dontChoose");
+        }
+        
+        
+        public void lose() {   
+            undefeated = false;
+            losses++;
+            
+            show("lose");
+
+            if (losses >= 2) eliminate();
+        }
+        
+        public String toString() {
+            return "Team " + number + ":"
+                    + "\t undefeated = " + undefeated
+                    + "\t losses =     " + losses
+                    + "\t eliminated = " + eliminated
+                    + "\t chosen =     " + chosen; 
+        }
+        
+        private void show(String text) {
+//            System.out.println("~~~" + text + toString());            
         }
         
     }
